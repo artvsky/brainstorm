5495624233ed54df316fcba8ba5d78a5f44f1ed8
vernadsky27 <55432148+vernadsky27@users.noreply.github.com>
2019-10-28 22:12:02 +0300

    Update gun.py
    Удалён # перед print (6 строка), 8-12 строки приведены к PeP8, строки в классе ball приведены к PeP8, добавлена табуляция

--- gun.py
+++ gun.py
@@ -3,68 +3,54 @@ import tkinter as tk
 import math
 import time
 
-# print (dir(math))
+print (dir(math))
 
-root = tk.Tk()
-fr = tk.Frame(root)
+root=tk.Tk()
+fr=tk.Frame(root)
 root.geometry('800x600')
-canv = tk.Canvas(root, bg='white')
+canv=tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
 
-
 class ball():
-    def __init__(self, x=40, y=450):
-        """ Конструктор класса ball
+	def __init__(self, x=40, y=450):
+		"""
+		Конструктор класса ball
+		Args:
+		x - начальное положение мяча по горизонтали
+		y - начальное положение мяча по вертикали
+		"""
+		self.x=x
+		self.y=y
+		self.r=10
+		self.vx=0
+		self.vy=0
+		self.color=choice(['blue', 'green', 'red', 'brown'])
+		self.id=canv.create_oval(self.x-self.r, self.y-self.r, self.x+self.r, self.y+self.r, fill=self.color)
+		self.live=30
 
-        Args:
-        x - начальное положение мяча по горизонтали
-        y - начальное положение мяча по вертикали
+	def set_coords(self):
+		canv.coords(self.id, self.x-self.r, self.y-self.r, self.x+self.r, self.y+self.r)
+
+	def move(self):
+		"""
+		Переместить мяч по прошествии единицы времени.
+		Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
+		self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
+		и стен по краям окна (размер окна 800х600).
         """
-        self.x = x
-        self.y = y
-        self.r = 10
-        self.vx = 0
-        self.vy = 0
-        self.color = choice(['blue', 'green', 'red', 'brown'])
-        self.id = canv.create_oval(
-                self.x - self.r,
-                self.y - self.r,
-                self.x + self.r,
-                self.y + self.r,
-                fill=self.color
-        )
-        self.live = 30
+		# FIXME
+		self.x+=self.vx
+		self.y-=self.vy
 
-    def set_coords(self):
-        canv.coords(
-                self.id,
-                self.x - self.r,
-                self.y - self.r,
-                self.x + self.r,
-                self.y + self.r
-        )
-
-    def move(self):
-        """Переместить мяч по прошествии единицы времени.
-
-        Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
-        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
-        и стен по краям окна (размер окна 800х600).
-        """
-        # FIXME
-        self.x += self.vx
-        self.y -= self.vy
-
-    def hittest(self, obj):
-        """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
-
-        Args:
-            obj: Обьект, с которым проверяется столкновение.
-        Returns:
-            Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
-        """
-        # FIXME
-            return False
+	def hittest(self, obj):
+		"""Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
+		Args:
+		obj: Обьект, с которым проверяется столкновение.
+		Returns:
+		Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
+		"""
+		# FIXME
+			return False
 
 class gun():
 	def __init__(self):
